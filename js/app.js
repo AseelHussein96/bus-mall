98254245b4217ba79670484747bcb025f6ccc96a
AseelHussein96 <aseelayed1996@gmail.com>
2021-03-16 08:56:40 +0200

    add chart

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use straict';
-const products =[
+const products = [
     'bag',
     'banana',
     'bathroom',
@@ -22,27 +22,25 @@ const products =[
     'water-can',
     'wine-glass',
 ];
-const busMarket=document.getElementById('busMarket');
-const leftImage=document.getElementById('leftImage');
-const CenterImage=document.getElementById('CenterImage');
-const RightImage=document.getElementById('RightImage');
+const busMarket = document.getElementById('busMarket');
+const leftImage = document.getElementById('leftImage');
+const CenterImage = document.getElementById('CenterImage');
+const RightImage = document.getElementById('RightImage');
 
-function market(product){
-    this.product=product;
-    this.views=0;
-    this.votes=0;
-    this.path=`./img/${product}.jpg`
+function market(product) {
+    this.product = product;
+    this.views = 0;
+    this.votes = 0;
+    this.path = `./img/${product}.jpg`
     market.all.push(this);
-    
+
 }
-market.all=[];
-for (let i=0;i<products.length;i++){
+market.all = [];
+for (let i = 0; i < products.length; i++) {
     new market(products[i]);
-    const table=document.getElementById('NUmberOfCilck');
-    
 }
 console.table(market.all)
-   
+
 function render(){
     const leftIndex=randomNumber(0, market.all.length-1);
     const leftrandomMarket=market.all[leftIndex];
@@ -68,27 +66,110 @@ function render(){
 }
 
 
-busMarket.addEventListener('click',click);
-function click(event){
-    if(event.target.id === 'leftImage' || event.target.id === 'CenterImage' || event.target.id === 'RightImage'){
-        for (let i=0;i<market.all.length;i++){
-            if (market.all[i].product === event.target.title){
+busMarket.addEventListener('click', click);
+
+let maxTrials=25;
+
+function click(event) {
+    maxTrials-= 1;
+
+    if (event.target.id === 'leftImage' || event.target.id === 'CenterImage' || event.target.id === 'RightImage') {
+        for (let i = 0; i < market.all.length; i++) {
+            if (market.all[i].product === event.target.title) {
                 market.all[i].votes++;
                 market.all[i].views++;
                 console.table(market.all[i])
+
+
             }
-          }
-          
+
+        }
+        render ();
     }
-    render();
+
+    if(maxTrials===0) {
+        busMarket.removeEventListener('click', click);
+        createChart();
+
+    }
+}
+let button=document.getElementById('button');
+busMarket.addEventListener('click', click);
+
+function clickButton(){
+  let List=document.getElementById('ul');
+  for (let j=0;j<market.all.length;j++){
+    let li = document.createElement('li');
+    li.innerText='This product  ' +market.all[j].product+' has '+market.all[j].votes+' votes and '+market.all[j].views+' views .';
+    List.appendChild(li);
+    console.log(market.all[j].votes,market.all[j].views);
+  }
 }
 
-
-function randomNumber(min,max){
-    return Math.floor(Math.random()*(max - min +1)) +min; 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-render();
 
-
+function createChart(){
+    let context = document.getElementById('myChart').getContext('2d');
+    let getproducts=[];
+    let getvotes=[];
+    let getview=[];
+  
+    for(let i=0;i<market.all.length;i++){
+        getproducts.push(market.all[i].product);
+    }
+    for(let i=0;i<market.all.length;i++){
+        getvotes.push(market.all[i].votes);
+    }
+    for(let i=0;i<market.all.length;i++){
+        getview.push(market.all[i].views);
+    }
+    let chartObject={
+      
+      type: 'bar',
+      
+      data: {
+          labels:getproducts,
+          datasets: [{
+              label: 'getvotes',
+              backgroundColor: 'rgb(100, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data:getvotes 
+          }
+        ]
+      },
+      data: {
+        labels:getproducts,
+        datasets: [{
+            label: 'getvotes',
+            backgroundColor: 'rgb(100, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data:getvotes 
+        }
+      ]
+    },
+    // // data: {
+    // //     labels:getproducts,
+    // //     datasets: [{
+    // //         label: 'getviews',
+    // //         backgroundColor: 'RGB(255, 73, 112)',
+    // //         borderColor: 'RGB(255, 149, 172)',
+    // //         data:getvotes 
+    // //     }
+    // //   ]
+    // },
+      options: {
+        scales: {
+          xAxes: [{
+              barPercentage: 0.4
+          }]
+      }
+      }
+  }
+    let chart = new Chart(context,chartObject);
+    
+  }
+render ();
 
 
